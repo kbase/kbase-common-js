5a57c001d2c84523c31bf2b9802d2fb5c5a0e6e3
Erik Pearson <eapearson@lbl.gov>
2015-10-05 08:16:57 -0700

    never mind

--- src/js/dom.js
+++ src/js/dom.js
@@ -7,42 +7,6 @@
  */
 define([], function () {
     'use strict';
-    var eventsPendingAttachment = [],
-        eventsAttached = [];
-
-    // DOM EVENTS
-    function addEvent(type, handler, id, data) {
-        if (!id) {
-            id = html.genId();
-        }
-        var event = {
-            type: type,
-            selector: '#' + id,
-            nodeId: id,
-            handler: handler
-        };
-        eventsPendingAttachment.push(event);
-        return id;
-    }
-    function attachEvents() {
-        eventsPendingAttachment.forEach(function (event) {
-            event.node = dom.nodeForId(event.nodeId);
-            event.listener = event.node.addEventListener(event.type, event.handler);
-            eventsAttached.push(event);
-        });
-        eventsPendingAttachment = [];
-    }
-    function detachEvents() {
-        eventsAttached.forEach(function (event) {
-            if (event.listener) {
-                event.node.removeEventListener(event.type, event.handler);
-                delete event.listener;
-            }
-        });
-        eventsAttached = [];
-    }
-
-
     function createElement(name) {
         return document.createElement(name);
     }
@@ -63,13 +27,13 @@ define([], function () {
     }
     function qs(selector) {
         return document.querySelector(selector);
-    }
+    }    
     function setHtml(nodeOrSelector, content) {
         var node;
         if (typeof nodeOrSelector === 'string') {
             node = qs(nodeOrSelector);
             if (node === null) {
-                throw new Error('No node found for selector "' + nodeOrSelector + '"');
+                throw new Error('No node found for selector "'+nodeOrSelector+'"');
             }
         } else {
             node = nodeOrSelector;
@@ -78,7 +42,7 @@ define([], function () {
         return node;
     }
 
-    return Object.freeze({
+    return {
         createElement: createElement,
         appendRoot: appendRoot,
         append: append,
@@ -88,9 +52,6 @@ define([], function () {
         findById: findById,
         nodeForId: nodeForId,
         getById: findById,
-        qs: qs,
-        addEvent: addEvent,
-        attachEvents: attachEvents,
-        detachEvents: detachEvents
-    });
+        qs: qs
+    };
 });
