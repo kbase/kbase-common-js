197b32193bb94ad3b83b7ac893b845039332ad6c
Erik Pearson <eapearson@lbl.gov>
2015-10-16 16:14:43 -0700

    code improvement (better usage of promises)

--- src/js/widgetMount.js
+++ src/js/widgetMount.js
@@ -26,92 +26,70 @@ define([
             container = dom.createElement('div');
             container = mount.appendChild(container);
             container.id = html.genId();
-            // container = dom.append(mount, dom.createElement('div'));
 
             function mountWidget(widgetId, params) {
                 // stop the old one
-                    // Stop and unmount current widget.
-                    return Promise.try(function () {
-                        if (mountedWidget) {
-                            var widget = mountedWidget.widget;
-                            return widget.stop()
-                                .then(function () {
-                                    return widget.detach();
-                                })
-                                .then(function () {
-                                    if (widget.destroy) {
-                                        return widget.destroy();
-                                    }
-                                });
+                // Stop and unmount current widget.
+                return Promise.try(function () {
+                    if (mountedWidget) {
+                        var widget = mountedWidget.widget;
+                        return widget.stop()
+                            .then(function () {
+                                return widget.detach();
+                            })
+                            .then(function () {
+                                if (widget.destroy) {
+                                    return widget.destroy();
+                                }
+                            });
+                    }
+                })
+                    .then(function () {
+                        // return runtime.ask('widgetManager', 'makeWidget', widgetId);
+                        return runtime.getService('widget').makeWidget(widgetId, {});
+                    })
+                    .then(function (widget) {
+                        if (widget === undefined) {
+                            throw new Error('Widget could not be created: ' + widgetId);
+                        }
+                        mountedWidget = {
+                            id: html.genId(),
+                            widget: widget,
+                            container: null,
+                            state: 'created'
+                        };
+                        return [widget, Promise.try(function () {
+                            if (widget.init) {
+                                return widget.init();
+                            }
+                        })];
+                    })
+                    .spread(function (widget) {
+                        var c = dom.createElement('div');
+                        c.id = mountedWidget.id;
+                        container.innerHTML = '';
+                        dom.append(container, c);
+                        mountedWidget.container = c;
+                        return [widget, widget.attach(c)];
+                    })
+                    .spread(function (widget) {
+                        if (widget.start) {
+                            return [widget, widget.start(params)];
                         }
                     })
-                        .then(function () {
-                            // return runtime.ask('widgetManager', 'makeWidget', widgetId);
-                            return runtime.getService('widget').makeWidget(widgetId, {
-                                runtime: runtime
-                            });
-                        })
-                        .then(function (widget) {
-                            if (widget === undefined) {
-                                throw new Error('Widget could not be created: ' + widgetId);
-                            }
-                            mountedWidget = {
-                                id: html.genId(),
-                                widget: widget,
-                                container: null,
-                                state: 'created'
-                            };
-
-                            /* TODO: config threaded here? */
-                            // init method is optional
-                            return Promise.try(function () {
-                                if (widget.init) {
-                                    return widget.init();
-                                }
-                            })
-                        })
-                        .then(function () {
-                            var c = dom.createElement('div');
-                            c.id = mountedWidget.id;
-                            container.innerHTML = '';
-                            dom.append(container, c);
-                            mountedWidget.container = c;
-                            return widget.attach(c);
-                        })
-                                .then(function () {
-                                    if (widget.start) {
-                                        return widget.start(params);
-                                    }
-                                })
-                                .then(function () {
-                                    if (widget.run) {
-                                        return widget.run(params);
-                                    }
-                                })
-                                .then(function () {
-                                    resolve();
-                                })
-                                .catch(function (err) {
-                                    console.log('ERROR initializing widget');
-                                    console.log(err);
-                                    reject(err);
-                                });
-
-                        })
-                        .catch(function (err) {
-                            console.log('ERROR');
-                            console.log(err);
-                            reject(err);
-                        });
-                });
+                    .spread(function (widget) {
+                        if (widget.run) {
+                            return widget.run(params);
+                        }
+                    });
             }
             return {
                 mountWidget: mountWidget
             };
         }
-        return {
-            make: function (config) {
-                return factory(config);
-            }
-        };
-    });
+    return {
+        make: function (config) {
+            return factory(config);
+        }
+    };
+});
