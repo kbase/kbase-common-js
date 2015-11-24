a047c1be3587d66cb7963b000edc6c4545bf2869
Erik Pearson <eapearson@lbl.gov>
2015-11-23 16:14:14 -0800

    repackage files into kb/common

---
+++ dist/kb/common/state.js
@@ -0,0 +1,40 @@
+define([
+    'kb_common_props'
+],
+    function (Props) {
+        'use strict';
+        function stateful() {
+            var state = Props.make(),
+                dirty = false;
+
+            function set(prop, value) {
+                state.setItem(prop, value);
+                dirty = true;
+            }
+            function get(prop) {
+                return state.getItem(prop);
+            }
+            function has(prop) {
+                return state.hasItem(prop);
+            }
+            function isDirty() {
+                return dirty;
+            }
+            function setClean() {
+                dirty = false;
+            }
+            return {
+                set: set,
+                get: get,
+                has: has,
+                isDirty: isDirty,
+                setClean: setClean
+            };
+        }
+        
+        return {
+            make: function () {
+                return stateful();
+            }
+        };
+    });
